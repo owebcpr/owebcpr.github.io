700fe7f7bb2256aaddeaaf9ff3eee9059b277b2a
Dariya <ameli.first@gmail.com>
2025-08-09 12:53:20 +0300

    add state

---
+++ pages/ukrainian-state/script.js
@@ -0,0 +1,14 @@
+﻿document.querySelectorAll('ul li').forEach(item => {
+    item.addEventListener('click', () => {
+        const parent = item.parentElement;
+        if (parent.classList.contains('answered')) return;
+        parent.classList.add('answered');
+
+        if (item.dataset.correct === "true") {
+            item.classList.add('correct');
+        } else {
+            item.classList.add('wrong');
+            parent.querySelector('[data-correct="true"]').classList.add('correct');
+        }
+    });
+});
