3fca99888fc14b39421f8f5be4aef4b34666bf7c
Dariya <ameli.first@gmail.com>
2025-08-09 12:30:48 +0300

    add page europa

---
+++ pages/europe/script.js
@@ -0,0 +1,16 @@
+﻿document.querySelectorAll('ul li').forEach(item => {
+    item.addEventListener('click', () => {
+        const parent = item.parentElement;
+        // Забороняємо повторний вибір у цьому питанні
+        if (parent.classList.contains('answered')) return;
+        parent.classList.add('answered');
+
+        if (item.dataset.correct === "true") {
+            item.classList.add('correct');
+        } else {
+            item.classList.add('wrong');
+            // підсвічуємо правильну відповідь
+            parent.querySelector('[data-correct="true"]').classList.add('correct');
+        }
+    });
+});
